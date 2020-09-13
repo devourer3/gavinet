9e6f51b8afe7a14b5d7cf01d4a365726537d532a
gibeom <gibeom30@daum.net>
2020-09-13 11:43:08 +0900

    Updates

---
+++ precache-manifest.75bd3198607df27fe6dbce4382080d38.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d97bf76963970d60fcf6db3ad1c93429",
+    "url": "/gavinet/index.html"
+  },
+  {
+    "revision": "5a896732725870bc5767",
+    "url": "/gavinet/static/css/main.1f855353.chunk.css"
+  },
+  {
+    "revision": "4fefe1ef3332e675974e",
+    "url": "/gavinet/static/js/2.cf01616d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/gavinet/static/js/2.cf01616d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a896732725870bc5767",
+    "url": "/gavinet/static/js/main.b91cf52d.chunk.js"
+  },
+  {
+    "revision": "99c6aa1b39a80caaa922",
+    "url": "/gavinet/static/js/runtime-main.2be40528.js"
+  },
+  {
+    "revision": "84b1411d20d2c23ca4e92a4f6d6f69dd",
+    "url": "/gavinet/static/media/close.84b1411d.svg"
+  },
+  {
+    "revision": "e07981d79c606524272d0619105184ef",
+    "url": "/gavinet/static/media/memo.e07981d7.svg"
+  },
+  {
+    "revision": "96c489313b6c0000d3a5de82e7b094b8",
+    "url": "/gavinet/static/media/refresh.96c48931.svg"
+  }
+]);
